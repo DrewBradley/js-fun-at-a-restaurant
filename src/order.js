6986aa604f2efc1c0cb595d83582c3b598f0893d
Drew Bradley <williamandrewb@gmail.com>
2020-10-08 14:32:51 -0600

    Update order.js

--- src/order.js
+++ src/order.js
@@ -1,16 +1,35 @@
 function takeOrder(orderNumber, arr) {
   if (arr.length < 3) {
     arr.push(orderNumber)
-  }
+  };
+};
+
+function refundOrder(orderNumber, arr) {
+  arr.splice((orderNumber - 1), 1)
+};
+
+function listItems(arr){
+  var string = '';
+  for (var i  = 0; i < arr.length-1; i++) {
+    string += (arr[i].item + ", ") ;
+  };
+  string += arr[arr.length-1].item;
+  return string;
+};
+
+function searchOrder(arr, str) {
+  for (var i = 0; i < arr.length; i++){
+    if (arr[i].item === str){
+      return true
+    }  
+  } 
+  return false;
 }
 
-function refundOrder() {
-
-}
 
 module.exports = {
   takeOrder,
   refundOrder,
-  // listItems,
-  // searchOrder
+  listItems,
+  searchOrder
 }
