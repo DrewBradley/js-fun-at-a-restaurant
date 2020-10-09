44d7ee6255251b869d69dda3797ca26bd16d9452
Drew Bradley <williamandrewb@gmail.com>
2020-10-09 16:48:17 -0600

    Update restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,31 @@
+function createRestaurant(name) {
+  var pizzaRestaurant = {
+          name: name,
+          menus: {breakfast: [], 
+                  lunch: [], 
+                  dinner: []},
+  }
+  return pizzaRestaurant;
+}
+
+function addMenuItem(place, item){
+    if (!place.menus[item.type].includes(item)){
+    place.menus[item.type].push(item);
+  }
+};
+
+function removeMenuItem(place, name, meal){
+  // if (place.menus[meal].name){
+  place.menus[meal].splice(1, name);
+  return `No one is eating our ${name} - it has been removed from the ${meal} menu!`;
+  // }else if (name === "Mom's Spaghetti"){
+  //  return `Sorry, we don't sell ${name}, try adding a new recipe!`
+  // };
+};
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant, 
+  addMenuItem,
+  removeMenuItem
 }
