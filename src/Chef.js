4d16030c204626b1eab0079d99356c261d45a6a4
Drew Bradley <williamandrewb@gmail.com>
2020-10-11 16:48:51 -0600

    Update Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -12,22 +12,15 @@ class Chef {
     }
   }
 
-  checkForFood(foodItem){
-    if (foodItem.name){
-      return `Yes, we're serving ${foodItem.name} today!`
-    } else {
-    return `Sorry, we aren't serving ${foodItem.name} today.`
+  checkForFood(foodName){
+    var foodProperty = foodName;
+    var passedFood = foodName.hasOwnProperty(foodProperty)
+    if (passedFood){
+      return `Yes, we're serving ${foodName.name} today!`
     }
+    return `Sorry, we aren't serving ${foodName.name} today.` 
   }
-}
+};
 
-// function removeMenuItem(eateryName, name, meal){
-//   var passedItem = (food) => (food.name === name);
-//   if (eateryName.menus[meal].some(passedItem)){
-//   eateryName.menus[meal].splice(name, 1);
-//   return `No one is eating our ${name} - it has been removed from the ${meal} menu!`;
-//   }else{
-//    return `Sorry, we don't sell ${name}, try adding a new recipe!`
-//   };
 
 module.exports = Chef;
